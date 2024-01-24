bb554b296a49548abd1d742dac39dbd2d1199793
matterwoven <145516753+matterwoven@users.noreply.github.com>
2024-01-24 13:21:06 -0800

    Add project files.

---
+++ GA_MethodOverloading_MatthewVargas/MethodOverloading.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GA_MethodOverloading_MatthewVargas
+{
+    internal class MethodOverloading
+    {
+        List<string> playerlist = new List<string>();
+
+        public List<string> Playerlist { get { return playerlist; } }
+
+        List<int> scorelist = new List<int>();
+
+        public List<int> Scorelist { get { return scorelist; } }
+
+        // Above are separated pieces of scores and player names
+
+        List<ListedPlayer> overallPlayersList = new List<ListedPlayer>();
+
+        // List for combined data, Players and Score
+
+        public class ListedPlayer
+        {
+            public string Name { get; set; }
+            public int Score { get; set; }
+        }
+        public List<ListedPlayer> GetOverallPlayersList()
+        {
+            return overallPlayersList;
+        }
+
+        public void AddRecentToList()
+        {
+            int lastIndex = playerlist.Count - 1;
+
+            overallPlayersList.Add(new ListedPlayer
+            {
+                Name = playerlist[lastIndex],
+                Score = scorelist[lastIndex]
+            });
+        }
+
+        public void NoteRecentPlayer(string newname, int newscore)
+        {
+            if (!string.IsNullOrEmpty(newname) && newscore > 0)
+            {
+                Playerlist.Add(newname);
+                Scorelist.Add(newscore);
+
+                AddRecentToList();
+
+                Console.WriteLine("Player added: " + newname + " | Score: " + newscore);
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Invalid name or score, please make a valid input");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ GA_MethodOverloading_MatthewVargas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+using System.Transactions;
+
+namespace GA_MethodOverloading_MatthewVargas
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to view the scores of your players?");
+            Console.WriteLine("Type Y / N");
+            if (Console.ReadLine() == "Y" )
+            {
+                MethodOverloading methodOverloading = new MethodOverloading();
+
+                // Note recent players
+                methodOverloading.NoteRecentPlayer("Matthew", 100);
+                methodOverloading.NoteRecentPlayer("Jose", 1000); 
+                methodOverloading.NoteRecentPlayer("Lilith", 670);
+                //methodOverloading.NoteRecentPlayer("22", -4555);
+                //methodOverloading.NoteRecentPlayer(22, 40);
+                
+                //^Invalid input examples commented
+
+                Console.WriteLine("");
+                Console.WriteLine("Current players:");
+                foreach (var player in methodOverloading.GetOverallPlayersList())
+                {
+                    
+                    Console.WriteLine($" - Player: {player.Name} | Score: {player.Score}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Okay then, have a great day");    
+            
+            } 
+
+
+        }
+    }
+}
